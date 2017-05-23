a6a20872605b05abb4fb1f25fb616975210f6270
Zubin Mehta <zubin.mehta1996@gmail.com>
2017-05-23 13:16:32 +0000

    Added middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 4000,
+  port = process.env.PORT || 4200,
   mongoose = require('mongoose'),
   Task = require('./api/models/model'),
   bodyParser = require('body-parser');
@@ -12,7 +12,9 @@ mongoose.connect('mongodb://localhost/sampledb');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 var routes = require('./api/routes/route');
 routes(app);
