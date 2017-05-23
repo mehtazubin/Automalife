8c180c678281bef42e3e5b42db3f70b4c43da6fc
mehtazubin <zubin.mehta1996@gmail.com>
2017-05-23 09:27:32 -0500

    Fix for delete user
    Updates deletion to only occur if the user is found in the database

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -44,12 +44,23 @@ exports.update_user = function(req, res) {
 
 
 exports.delete_user = function(req, res) {
-
-  User.remove({
-    username: req.params.username
-  }, function(err, user) {
+  var query = {};
+  query['username'] = req.params.username; 
+  User.findOne(query, function(err, user) {
     if (err)
       res.send(err);
-    res.json({ message: 'User successfully deleted' });
+    else{
+      if(user != null) {
+        User.remove({username: req.params.username}, function(err, user) {
+          if (err){
+            res.send(err);
+          }
+          res.json({ message: 'User successfully deleted' });
+        });
+      }
+      else {
+        res.json({message: 'User does not exist'});
+      }
+    }
   });
 };
