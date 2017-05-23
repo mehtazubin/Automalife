9ed2fd6ab5346c33983195124d5a135fd4b4344d
mehtazubin <zubin.mehta1996@gmail.com>
2017-05-23 08:02:16 -0500

    Switched to MVC file structure

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('test', function(res,req,next){
-	res.end({"work":"Success"});
-});
-
-module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 4000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/model'),
+  bodyParser = require('body-parser');
+  cors = require('cors');
+  
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/sampledb'); 
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/route');
+routes(app);
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
